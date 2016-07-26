efc1c13c0bfeb198932953bea51139f16a09f19d
Amy Campbell <acampbell@olchs.org>
2016-07-26 14:30:09 -0500

    Added Label outlet, and variable.

--- GitTesting/ViewController.swift
+++ GitTesting/ViewController.swift
@@ -4,13 +4,23 @@
 //
 //  Created by Amy Campbell on 7/26/16.
 //  Copyright © 2016 Amy Campbell. All rights reserved.
-//
+
+//testing GitHub
+
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelTest: UILabel!
+   
 
     override func viewDidLoad() {
+        
+        var name: String!
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -22,4 +32,7 @@ class ViewController: UIViewController {
 
 
 }
+
+
+
 
